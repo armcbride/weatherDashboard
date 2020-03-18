8d527169f4c51dbc87e5b740af7e072d3b853a76
Annaruth McBride <armcbride17@gmail.com>
2020-03-18 12:30:09 -0500

    began local storage function and added forecast API

--- develop/app.js
+++ develop/app.js
@@ -1,5 +1,9 @@
 
 //global variables
+
+//variable to place the user input into queryURL
+var city = $('#search-term').val();
+//weather queryURL
 var APIKey = "4b4fc905987791628273a594a06e68d7";
 var baseURL = 'https://api.openweathermap.org/data/2.5/weather?';
 
@@ -8,12 +12,29 @@ var weatherIconBase = `http://openweathermap.org/img/wn/`;
 var currentTime = moment().format('L');
 console.log(currentTime);
 
-// var cityHistory = JSON.parse(localStorage.getItem("cityHistory")) || [];
+//five day forecast queryURL
+var forecastKey = "327c22d7e329579cb0b1376b7c47c4a1";
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?" ;
+
+function getForecast(){
+
+  $.ajax({
+    url: `${forecastURL}q=${city}&appid=${forecastKey}`,
+    method: "GET"
+  }).then(function (res) {
+    displayInfo(res)
+    saveCity(res.name)
+    console.log(res);
+   
+  })
+}
+var cityHistory = JSON.parse(localStorage.getItem("cityHistory")) || [];
+
 
 // search term function
 function handleSearch(e) {
   e.preventDefault()
-  var city = $('#search-term').val();
+  
   $.ajax({
     url: `${baseURL}q=${city}&appid=${APIKey}`,
     method: "GET"
@@ -34,7 +55,9 @@ var feelsTemp= (res.main.feels_like - 273.15) * 1.8 + 32;
       <span><h3>${res.name}</h3></span>
       <img src="${weatherIconBase}${res.weather[0].icon}@2x.png"
     </div>
-    <div id='display-details'><h4>Temperature: ${tempF.toFixed(2)}<br>
+    <div id='display-details'><h4>
+    ${currentTime}<br>
+    Temperature: ${tempF.toFixed(2)}<br>
 Feels like: ${feelsTemp.toFixed(2)}<br>
     Humidity: ${res.main.humidity}%<br>
     Wind Speed: ${res.wind.speed}mph <br></h4></div>
@@ -46,7 +69,8 @@ Feels like: ${feelsTemp.toFixed(2)}<br>
 
 //local storage function
 function saveCity(name) {
-
+  localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
 }
 
 $('#search-form').submit(handleSearch)
+$('#forecast-display').prepend(getForecast)
